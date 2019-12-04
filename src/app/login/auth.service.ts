ca43b34d0306e8314593c9e8d2ae22e24557bdfa
urbanaleksandra <urb.saska@gmail.com>
2019-12-04 17:18:23 +0100

    fix

--- src/app/login/auth.service.ts
+++ src/app/login/auth.service.ts
@@ -42,7 +42,9 @@ export class AuthService {
       console.log('Login success');
       this.username = username;
       this.password = password;
-      this.access_token = res; //ako je na serveru u beku dobro return-ovan token, onda je on ovde
+      this.access_token = res.body;
+      console.log(res);
+      console.log(res.accessToken); //ako je na serveru u beku dobro return-ovan token, onda je on ovde
       console.log(this.access_token);
       this.registerSuccessfulLogin(username, password);
     }));
@@ -72,5 +74,13 @@ export class AuthService {
     let user = sessionStorage.getItem(this.USER_NAME_SESSION_ATTRIBUTE_NAME)
     if (user === null) return ''
     return user
+  }
+
+  tokenIsPresent() {
+    return this.access_token != undefined && this.access_token != null;
+  }
+
+  getToken() {
+    return this.access_token;
   }
 }
