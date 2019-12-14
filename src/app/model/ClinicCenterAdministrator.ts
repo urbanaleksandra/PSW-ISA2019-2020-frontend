50a6a7b75fcb36e4b685f1847ef8fcb366f58101
Tamara Jovicic <57210311+tacajo@users.noreply.github.com>
2019-12-14 01:46:50 +0100

    bugfix

--- src/app/model/ClinicCenterAdministrator.ts
+++ src/app/model/ClinicCenterAdministrator.ts
@@ -2,4 +2,22 @@ export class ClinicCenterAdministrator{
     username : string = "";
 
     password : string = "";
+
+    firstName : string = "";
+		
+    lastName : string = "";
+
+    email : string = "";
+
+    address : string = "";
+
+    city : string = "";
+
+    country : string = "";
+
+    mobileNumber : number;
+
+    jmbg : number;
+
+    firstLog: number = 0;
 }
