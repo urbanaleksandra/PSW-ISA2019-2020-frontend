92dfec80f774021cd9aeb0cb236707b92161e5e5
zeljana2309 <57210469+zeljana2309@users.noreply.github.com>
2019-12-14 16:36:04 +0100

    added changing password

--- src/app/profiles/doctor.component.ts
+++ src/app/profiles/doctor.component.ts
@@ -4,81 +4,94 @@ import { Doctor } from '../model/Doctor';
 import { PatientService } from '../service/patient.service';
 
 @Component({
-    templateUrl: './doctor.component.html',
-    styleUrls: ['./clinic-center-administrator.component.css']
+  templateUrl: './doctor.component.html',
+  styleUrls: ['./clinic-center-administrator.component.css']
 })
 
-export class DoctorComponent{
-    user:User=new User() ;
-    doctor: Doctor=new Doctor();
-    newDoctor: Doctor= new Doctor();
-    constructor(private doctorService : PatientService) { }
+export class DoctorComponent {
+  user: User = new User();
+  invalidLogin = false;
+  errorMessage = 'Invalid Credentials';
+  doctor: Doctor = new Doctor();
+  newDoctor: Doctor = new Doctor();
+  constructor(private doctorService: PatientService) { }
 
-    ngOnInit() {
-     this.getDoctor();
-      
-    }
-  
-    private isButtonVisible = false;
+  ngOnInit() {
+    this.getDoctor();
 
-    private isButtonVisible2 = false;
+  }
 
-    getDoctor(){
-      this.user.username = sessionStorage.getItem("authenticatedUser");
-      this.doctorService.getDoctor(this.user.username).subscribe(
-        data =>{
-          this.doctor=data;
+  private isButtonVisible = false;
+
+  private isButtonVisible2 = false;
+
+  getDoctor() {
+    this.user.username = sessionStorage.getItem("authenticatedUser");
+    this.doctorService.getDoctor(this.user.username).subscribe(
+      data => {
+        this.doctor = data;
       },
       error => {
-      console.log(error);
+        console.log(error);
       }
     )
+  }
+
+  change(isButtonVisible: boolean) {
+    if (this.isButtonVisible == false)
+      this.isButtonVisible = true;
+    else this.isButtonVisible = false;
+  }
+
+  change2(isButtonVisible2: boolean) {
+
+    if (this.isButtonVisible2 == false)
+      this.isButtonVisible2 = true;
+    else this.isButtonVisible2 = false;
+
+  }
+
+  submitNewInfo() {
+    this.invalidLogin=false;
+    this.isButtonVisible2 = false;
+    if (this.newDoctor.password === "") {
+      this.newDoctor.password = this.doctor.password;
+    } else if (this.newDoctor.oldPassword != this.doctor.password) {
+      this.invalidLogin = true;
+    }
+    if (this.newDoctor.firstName === "") {
+      this.newDoctor.firstName = this.doctor.firstName;
+    }
+    if (this.newDoctor.lastName === "") {
+      this.newDoctor.lastName = this.doctor.lastName;
+
+    }
+    if (this.newDoctor.city === "") {
+      this.newDoctor.city = this.doctor.city;
+    }
+    if (this.newDoctor.country === "") {
+
+      this.newDoctor.country = this.doctor.country;
+    }
+    if (this.newDoctor.address === "") {
+      this.newDoctor.address = this.doctor.address;
+    }
+    if (this.newDoctor.mobileNumber === NaN) {
+      this.newDoctor.mobileNumber = this.doctor.mobileNumber;
+    }
+    if (this.newDoctor.email === "") {
+      this.newDoctor.email = this.doctor.email;
     }
 
-    change(isButtonVisible:boolean){
-      if(this.isButtonVisible==false)
-      this.isButtonVisible=true;
-      else this.isButtonVisible=false;
-      }
+    this.newDoctor.username = this.doctor.username;
 
-      change2(isButtonVisible2:boolean){
 
-        if(this.isButtonVisible2==false)
-        this.isButtonVisible2=true;
-        else this.isButtonVisible2=false;
-      
-      }
+    if (this.invalidLogin == false) {
+      location.reload();
+      this.doctorService.changeDoctorInfo(this.newDoctor).subscribe()
 
-      submitNewInfo(){
-        this.isButtonVisible2=false;
-        if(this.newDoctor.firstName === ""){
-          this.newDoctor.firstName = this.doctor.firstName;
-        }
-         if(this.newDoctor.lastName === ""){
-          this.newDoctor.lastName = this.doctor.lastName;
-         
-        }
-        if(this.newDoctor.city === ""){
-          this.newDoctor.city = this.doctor.city;
-        }
-        if(this.newDoctor.country === ""){
-          
-          this.newDoctor.country = this.doctor.country;
-        }
-        if(this.newDoctor.address === ""){
-          this.newDoctor.address = this.doctor.address;
-        }
-        if(this.newDoctor.mobileNumber === NaN){
-          this.newDoctor.mobileNumber = this.doctor.mobileNumber;
-        } 
-        if(this.newDoctor.email === ""){
-          this.newDoctor.email = this.doctor.email;
-        }
-        this.newDoctor.username = this.doctor.username;
+    }
     
-        location.reload();
-    
-        this.doctorService.changeDoctorInfo(this.newDoctor).subscribe()
-      }
+  }
 }
 
