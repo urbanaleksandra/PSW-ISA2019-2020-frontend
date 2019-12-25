939db944c4fcee0cfec26e89f0c543f3cf38ffaf
Tamara Jovicic <57210311+tacajo@users.noreply.github.com>
2019-12-25 16:28:50 +0100

    added new component

---
+++ src/app/auth-recipe/auth-recipe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-recipe',
+  templateUrl: './auth-recipe.component.html',
+  styleUrls: ['./auth-recipe.component.css']
+})
+export class AuthRecipeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
