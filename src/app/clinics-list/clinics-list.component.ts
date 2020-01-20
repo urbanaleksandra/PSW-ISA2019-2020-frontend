cf128447453e7796d0d95c5b621637d2d04c1ec7
urbanaleksandra <urb.saska@gmail.com>
2020-01-20 17:16:46 +0100

    nove_izmene

--- src/app/clinics-list/clinics-list.component.ts
+++ src/app/clinics-list/clinics-list.component.ts
@@ -13,9 +13,12 @@ import { Appointment } from '../model/Appointment';
 export class ClinicsListComponent implements OnInit {
 
   clinic: Clinic = new Clinic();
+  clinic1: Clinic = new Clinic();
   appointment: Appointment = new Appointment();
   clinics: Clinic[] = [];
+  clinics1: Clinic[] = [];
   flagForSearch: boolean;
+  flagForSearchTable: boolean;
 
   constructor(private clinicService: ClinicService,
     private router: Router) { }
@@ -23,16 +26,20 @@ export class ClinicsListComponent implements OnInit {
   ngOnInit() {
     this.getClinics();
     this.flagForSearch = false;
+    this.flagForSearchTable = false;
   }
 
   onSubmit(){
     console.log(this.appointment.date);
     if(this.appointment.date == ""){
       this.flagForSearch = true;
+      this.flagForSearchTable = false;
     }
     else{
       // ovde odraditi sve za search
       this.flagForSearch = false;
+      this.flagForSearchTable = true;
+      this.getSearchClinics();
     }
     //console.log(this.appointment.type);
   }
@@ -41,6 +48,17 @@ export class ClinicsListComponent implements OnInit {
     this.clinicService.getClinics().subscribe(
         data =>{
             this.clinics=data;
+        },
+        error => {
+        console.log(error);
+        }
+       )
+  }
+
+  getSearchClinics(){
+    this.clinicService.getSearchClinics(this.appointment.date).subscribe(
+        data =>{
+            this.clinics1=data;
         },
         error => {
         console.log(error);
@@ -70,6 +88,29 @@ export class ClinicsListComponent implements OnInit {
  
       });
     }
+
+    sortData1(sort: Sort) {
+      const data = this.clinics1.slice();
+      if (!sort.active || sort.direction === '') {
+        this.clinics1 = data;
+        return; 
+      }
+     
+      this.clinics1 = data.sort((a, b) => {
+        const isAsc = sort.direction === 'asc';
+        switch (sort.active) {
+          case 'name': return compare(a.name, b.name, isAsc);
+          case 'address': return compare(a.address, b.address, isAsc);
+          case 'description': return compare(a.description, b.description, isAsc);
+          case 'pricelist': return compare(a.pricelist, b.pricelist, isAsc);
+          case 'profit': return compare(a.profit, b.profit, isAsc);
+          case 'id': return compare(a.id, b.id, isAsc);
+          default: return 0;
+        }
+  
+   
+        });
+      }
 
 }
 
